b387563c4790811afe760c38f9a1a33ebe1e039b
unknown <albertbe1569@gmail.com>
2023-07-03 13:08:28 +0530

    Exception Program

---
+++ src/com/tec/copy/Pattern_Program1.java
@@ -0,0 +1,24 @@
+package com.tec.copy;
+
+public class Pattern_Program1 {
+
+		public static void main(String[] args) {
+			
+			int star=6;
+			int space=-1;
+			for(int i=1; i<=5; i++) {
+				star--;
+				space++;
+				for(int j=1; j<=space; j++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=star; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+				//System.out.println();
+			}
+
+		}
+
+}
---
+++ src/com/tec/copy/Pattern_Program4.java
@@ -0,0 +1,23 @@
+package com.tec.copy;
+
+public class Pattern_Program4 {
+
+	public static void main(String[] args) {
+		int star=11;
+		int space=-1;
+		
+		for(int i=1; i<=5; i++) {
+			star-=2;
+			space++;
+			for(int j=1; j<=space; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=star; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
